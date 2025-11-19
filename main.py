a36bdd542be734aad1e4e0ffe5e697a8c1237b69
Roman E <rk2555@yandex.ru>
2025-11-19 13:53:51 +0300

    start_command, кнопка

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
-"""В телеграмме FirstBot"""
+"""NetologyFirst"""
 
 import telebot
 from dotenv import load_dotenv
 import os
+from telebot import types
 
 # Загружаем переменные из .env
 load_dotenv()
 
 token = os.getenv("TOKEN")
-name = 'Роман'
 bot = telebot.TeleBot(token)
 
 
-@bot.message_handler(content_types=["text"])
-def echo(message):
-    if name in message.text:
-        bot.send_message(message.chat.id, 'Привет Роман!')
-    else:
-        bot.send_message(message.chat.id, message.text)
+@bot.message_handler(commands=['start'])
+def start_command(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("Кнопка")
+    markup.add(item1)
+    bot.send_message(message.chat.id, 'Привет! Нажми на кнопку', reply_markup=markup)
+
+
+@bot.message_handler(content_types=['text'])
+def message_reply(message):
+    if message.text == "Кнопка":
+        bot.send_message(message.chat.id, "Была нажата кнопка")
 
 
 if __name__ == '__main__':
