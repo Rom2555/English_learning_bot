acddd8a9f94f1483caf17bd5656589829d288398
Roman E <rk2555@yandex.ru>
2025-11-20 22:50:48 +0300

    модуль работы со словами. Функции получения списков кортежей общих, пользовательских и всех слов

---
+++ words.py
@@ -0,0 +1,26 @@
+from database import get_connection
+
+def get_user_words(user_id):
+    conn = get_connection()
+    cur = conn.cursor()
+    try:
+        cur.execute("SELECT word, translation FROM user_words WHERE user_id = %s", (user_id,))
+        return cur.fetchall()
+    finally:
+        cur.close()
+        conn.close()
+
+def get_general_words():
+    conn = get_connection()
+    cur = conn.cursor()
+    try:
+        cur.execute("SELECT word, translation FROM general_words")
+        return cur.fetchall()
+    finally:
+        cur.close()
+        conn.close()
+
+def get_all_words(user_id):
+    user_words = get_user_words(user_id)
+    general_words = get_general_words()
+    return user_words + general_words
